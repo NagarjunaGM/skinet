820c1bf619de0cf7836e178c39d938a361f26179
NagarjunaGM <nagarjunagm@gmail.com>
2023-10-28 19:43:48 +0530

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
-
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
