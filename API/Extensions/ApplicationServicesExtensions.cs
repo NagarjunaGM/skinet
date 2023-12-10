c0541efb4a904d96c327929381252cc93292f950
NagarjunaGM <nagarjunagm@gmail.com>
2023-12-10 20:59:59 +0530

    End of Section 13

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -7,6 +7,8 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
@@ -22,7 +24,12 @@ namespace API.Extensions
             {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
-
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+               var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });
+            services.AddScoped<IBasketRepository,BasketRepository>();
             services.AddScoped<IProductRepository,ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -56,4 +63,5 @@ namespace API.Extensions
             return services;
         }
     }
+
 }
